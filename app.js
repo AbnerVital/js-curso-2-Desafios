f8dc9905dab97b46e87f1ffc8f2e1b96d9fd1da0
Abner Vital <avgchaves@gmail.com>
2025-01-29 13:18:19 -0300

    Criei uma função que é executada quando o botão prompt é clicado, no alert é perguntando o nome de uma cidade do Brasil. Em seguida, exibe um alerta com a mensagem concatenando a resposta com o texto: Estive em {cidade} e lembrei de você.

--- app.js
+++ app.js
@@ -8,3 +8,8 @@ function exibirMensagemNoConsole(){
 function exibirAlerta() {
     alert('Eu amo Js')
 }
+
+function cidadeDoBrasil (){
+    cidade = prompt ('Em qual cidade você nasceu? ')
+    alert(`Estive em ${cidade} e lembrei de você`)
+}
