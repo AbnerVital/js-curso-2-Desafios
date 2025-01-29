a79b3c8827930f81aa6401a420a3346773779052
Abner Vital <avgchaves@gmail.com>
2025-01-29 13:31:11 -0300

    criei o botão Soma que ao clicar, é solicitado 2 números inteiros, ele soma os dois numeros e informa o resultado atráves de um alert.

--- app.js
+++ app.js
@@ -13,3 +13,10 @@ function cidadeDoBrasil (){
     cidade = prompt ('Em qual cidade você nasceu? ')
     alert(`Estive em ${cidade} e lembrei de você`)
 }
+
+function somandoDoisNumeros() {
+    let primeiroNumero = parseInt(prompt('Digite o primeiro número'));
+    let segundoNumero = parseInt(prompt('Digite o segundo número'));
+    let resultado = primeiroNumero + segundoNumero;
+    alert(`${primeiroNumero} + ${segundoNumero} = ${resultado}`)
+}
